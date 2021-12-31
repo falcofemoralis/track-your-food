4f827d28ca2bbed459ae333a7bb310d467724f44
Falcofemoralis <araraunapappagallo@gmail.com>
2021-12-31 12:33:49 +0200

    Fixed bug

--- src/services/FoodService.js
+++ src/services/FoodService.js
@@ -4,7 +4,7 @@ import Food from '../objects/Food';
 
 const HTMLParser = require('fast-html-parser');
 export default class {
-    fileName = "foodList.txt";
+    fileName = "foodList2.txt";
     foodList;
     setFoodList;
 
@@ -47,27 +47,23 @@ export default class {
      * @returns {Food} - найденный объкт продукта
      */
     findFoodById = async (id) => {
-        try {
-            const res = await fetch(`https://barcode-list.ru/barcode/RU/%D0%9F%D0%BE%D0%B8%D1%81%D0%BA.htm?barcode=${id}`)
-            const html = await res.text()
-            const root = HTMLParser.parse(html);
+        const res = await fetch(`https://barcode-list.ru/barcode/RU/%D0%9F%D0%BE%D0%B8%D1%81%D0%BA.htm?barcode=${id}`)
+        const html = await res.text()
+        const root = HTMLParser.parse(html);
 
-            if (root) {
-                const nameEl = root.querySelector("table.randomBarcodes tr.even")
+        if (root) {
+            const nameEl = root.querySelector("table.randomBarcodes tr.even")
 
-                if (nameEl) {
-                    const row = nameEl.querySelectorAll("td")
+            if (nameEl) {
+                const row = nameEl.querySelectorAll("td")
 
-                    if (row.length > 0) {
-                        return new Food(row[2].rawText, null)
-                    }
+                if (row.length > 0) {
+                    return new Food(row[2].rawText, null)
                 }
             }
-
-            throw Error("not found")
-        } catch (error) {
-            console.error(error)
         }
+
+        throw Error("not found")
     }
 
 }
